6ed5df810a529e48b86e62fa0095de78dcda7063
Yasuhiro Yamaguchi <altescy@gmail.com>
2017-08-16 16:53:52 +0900

    fix file path

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ chsh -s `which zsh`
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 rm $HOME/.zshrc
-ln -s ./.zshrc $HOME/.zshrc
-ln -s ./.tmux.conf $HOME/.tmux.conf
+ln -s .zshrc $HOME/.zshrc
+ln -s .tmux.conf $HOME/.tmux.conf
