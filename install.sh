5bb9cdfb0c725738e164da56f27fdfc48e3381a5
altescy <altescy@gmail.com>
2017-12-10 14:45:27 +0900

    modify url

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ sudo apt install -y zsh tmux xsel fonts-powerline
 sudo chsh -s `which zsh`
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-curl -fsSL https://raw.githubusercontent.com/altescy/master/zsh-and-tmux/.zshrc > ~/.zshrc
-curl -fsSL https://raw.githubusercontent.com/altescy/master/zsh-and-tmux/.tmux.conf > ~/.tmux.conf
+curl -fsSL https://raw.githubusercontent.com/altescy/zsh-and-tmux/master/.zshrc > ~/.zshrc
+curl -fsSL https://raw.githubusercontent.com/altescy/zsh-and-tmux/master/.tmux.conf > ~/.tmux.conf
